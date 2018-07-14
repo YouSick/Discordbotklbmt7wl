5f5f834b01121441d21f0690d622e598302cac81
YouSick <41239192+YouSick@users.noreply.github.com>
2018-07-14 22:09:06 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,6 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
 });
  client.on("message", message => {
     var prefix = "+";
- 
             var args = message.content.substring(prefix.length).split(" ");
             if (message.content.startsWith(prefix + "clear")) {
    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('? | **لا يوجد لديك صلاحية لمسح الشات**');
