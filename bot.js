a1e4c3733c74a50a18c6d3734c42e95916ad4994
YouSick <41239192+YouSick@users.noreply.github.com>
2018-07-14 22:19:32 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '.'
@@ -27,5 +28,31 @@ client.user.setGame(`Listen To YouSick_ Orders`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+
+
+
+
+ client.on("message", message => {
+    var prefix = "+";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('? | **لا يوجد لديك صلاحية لمسح الشات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم مسح الشات",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل ",
+        footer: {
+          text: "©Ghost"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+}); 
 
 client.login(process.env.BOT_TOKEN);
