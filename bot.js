6c013da57c4ac390df0a24f4c3e07798f17ad938
YouSick <41239192+YouSick@users.noreply.github.com>
2018-07-17 22:48:38 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,9 +58,9 @@ client.user.setGame(`Listen To YouSick_ Orders`,"http://twitch.tv/S-F")
 
 client.on('message', message => {
     var args = message.content.split(/[ ]+/)
-    if(message.content.includes('rd 3la 1m7mmd_')){
+    if(message.content.includes('00100')){
         message.delete()
-    return message.reply(`**54 ya @1M7mmD_#7345 00100**`)
+    return message.reply(`**Dont Sweraing!**`)
    message.delete()
     }
 });
