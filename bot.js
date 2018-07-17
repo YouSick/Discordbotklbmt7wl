99ccbe4e2c5b27d727d75974f0fb0726c28e82c0
YouSick <41239192+YouSick@users.noreply.github.com>
2018-07-17 22:24:34 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,4 +117,22 @@ if(message.content === adminprefix + "restart") {
     }
   
   }); // By Ghost
+
+
+client.on('message', message => {
+    if (message.content.startsWith(".avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var Hamada = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${Hamada.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
 client.login(process.env.BOT_TOKEN);
