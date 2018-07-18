7964d23ec83c91b94964c7b977827576d8a92e86
YouSick <41239192+YouSick@users.noreply.github.com>
2018-07-18 16:17:33 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -204,37 +204,26 @@ if (message.content.startsWith(".kick")) {
 
 
 
-ient.on("message", message => {
-  if (message.author.bot) return;
-
-  let command = message.content.split(" ")[0];
-
-  if (command === "#mute") {
-        if (!message.member.hasPermission('MANAGE_ROLES')) return message.reply(" لا يوجد لديك برمشن 'Manage Roles' ").catch(console.error);
-  let user = message.mentions.users.first();
-  let modlog = client.channels.find('name', 'mod-log');
-  let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'Muted');
-  if (!muteRole) return message.reply(" لا يوجد رتبة الميوت 'Muted' ").catch(console.error);
-  if (message.mentions.users.size < 1) return message.reply(' يجب عليك منشنت شخص اولاً').catch(console.error);
-
-  const embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .addField('الأستعمال:', 'اسكت/احكي')
-    .addField('تم ميوت:', ${user.username}!${user.discriminator} (${user.id}))
-    .addField('بواسطة:', ${message.author.username}!${message.author.discriminator})
-
-   if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.reply(' لا يوجد لدي برمشن Manage Roles ').catch(console.error);
-
-  if (message.guild.member(user).roles.has(muteRole.id)) {
-return message.reply(":white_check_mark: .. تم اعطاء العضو ميوت").catch(console.error);
-} else {
-    message.guild.member(user).addRole(muteRole).then(() => {
-return message.reply(":white_check_mark: .. تم اعطاء العضو ميوت كتابي").catch(console.error);
-});
-  }
-
-};
-
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc1') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
 });
 client.login(process.env.BOT_TOKEN);
