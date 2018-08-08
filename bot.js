f693861a3f8673e0f04d62ec3af5e3483c6596ed
YouSick <41239192+YouSick@users.noreply.github.com>
2018-08-08 20:45:02 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Listen To YouSick_ Orders`,"http://twitch.tv/S-F")
+client.user.setGame(`Officer Mukhld`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
