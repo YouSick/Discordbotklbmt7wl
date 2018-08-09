73f18ad7cc6ebefb581896b452aa3daeae719c82
YouSick <41239192+YouSick@users.noreply.github.com>
2018-08-09 22:31:57 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -401,4 +401,50 @@ client.on("message", message => {
     }
 });
 
+var prefix = '$';
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'help')) {
+    let pages = ['الصفحه الاولي','الصفحه الثانيه','الصفحه الثالثه']
+    let page = 1;
+
+     embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setFooter(`Page ${page} of ${pages.length}`)
+    .setDescription(pages[page-1])
+
+    message.channel.sendEmbed(embed).then(msg => {
+
+        msg.react('◀').then( r => {
+            msg.react('▶')
+
+
+        const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀' && user.id === message.author.id;
+        const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶' && user.id === message.author.id;
+
+
+         backwards = msg.createReactionCollector(backwardsFilter, { time: 20000});
+        const forwards = msg.createReactionCollector(forwardsFilter, { time: 20000});
+
+
+
+        backwards.on('collect', r => {
+            if (page === 1) return;
+            page--;
+            embed.setDescription(pages[page-1]);
+            embed.setFooter(`Page ${page} of ${pages.length}`);
+            msg.edit(embed)
+        })
+        forwards.on('collect', r => {
+            (page === pages.length) return;
+            page++;
+            embed.setDescription(pages[page-1]);
+            embed.setFooter(`Page ${page} of ${pages.length}`);
+            msg.edit(embed)
+        })
+        })
+    })
+    }
+});
+
+
 client.login(process.env.BOT_TOKEN);
