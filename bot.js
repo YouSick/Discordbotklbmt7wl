2e6a9536572c79109256109cbdd6d49ad13442b0
YouSick <41239192+YouSick@users.noreply.github.com>
2018-08-10 16:04:24 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -326,16 +326,4 @@ client.on('message', message => {
 });
 
 
-if(message.content.startsWith(prefix + 'bot')) {
-        const embed = new Discord.RichEmbed()
-            .setColor("#00FFFF")
-            .setDescription(`**Servers**🌐 **__${client.guilds.size}__**
-**Users**👥 **__${client.users.size}__**
-**Channels**📚 **__${client.channels.size}__** `)
-        message.channel.sendEmbed(embed);
-    }
-
-});
-
-
 client.login(process.env.BOT_TOKEN);
