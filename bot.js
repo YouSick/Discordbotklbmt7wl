213a992082cff7e3dafc9125ec4c72c7e2ff2141
YouSick <41239192+YouSick@users.noreply.github.com>
2018-08-14 22:23:18 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -415,32 +415,4 @@ client.on('message', async message => {
           }
       });
 
-    bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-  if(cmd === `${prefix}botinfo`);
-  let bicon = bot.user.displyAvatarURL;
-  let botEmbed = new Discord.RichEmbed()
-
-.setDescription("Bot Information")
-.setcolor("#15f153")
-.setThumbnail(bicon)
-.addField("Bot Name", bot.user.username)
-.addField("Created on", bot.user.createdAt)
-.addField("Users", bot.users.size)
-.addField("Servers", bot.guilds.size)
-.addField("Channels", bot.channels.size)
-.addField("ID", bot.user.id)
-.addField("My Prefix", botconfig.token)
-.addField("My Language");
-
-    return message.channel.send(botEmbed);
-  }
-});
-
 client.login(process.env.BOT_TOKEN); 
