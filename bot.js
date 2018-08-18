917ae856fd904db0c4dbc21d3909cfd459246ada
YouSick <41239192+YouSick@users.noreply.github.com>
2018-08-18 15:42:38 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -497,5 +497,50 @@ client.on('message', msg => {
 
 });
 
+  hero.on('message',async message => {
+  function timeCon(time) {
+  let days = Math.floor(time % 31536000 / 86400);
+  let hours = Math.floor(time % 31536000 % 86400 / 3600);
+  let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60);
+  let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60);
+  days = days > 9 ? days : '0' + days;
+  hours = hours > 9 ? hours : '0' + hours;
+  minutes = minutes > 9 ? minutes : '0' + minutes;
+  seconds = seconds > 9 ? seconds : '0' + seconds;
+  return `${days > 0 ? `${days} Days ` : ''}${(hours || days) > 0 ? `${hours} Hours ` : ''}${minutes} Mins ${seconds} Secs`;
+  }
+  
+  if(message.author.bot) return;
+  if(message.channel.type === 'dm') return;
+  if(message.content.startsWith(prefix + "bot")) {
+    let ramUsage = (process.memoryUsage().rss / 1048576).toFixed();
+    let upTime = timeCon(process.uptime());
+    let createdAt = moment(hero.user.createdAt).fromNow();
+
+let m = await message.channel.send(`\`\`\`asciidoc\n= Normal Information =
+Creator :: ${hero.users.get("323885452207587329").username} - ${createdAt}
+Ping :: ${hero.pings[0]} ms
+UpTime :: ${upTime}
+
+= Servers Information =
+Servers :: ${hero.guilds.size}
+Users :: ${hero.users.size}
+Channels :: ${hero.channels.size}
+
+= Developer Information =
+NodeJS :: ${process.version}
+DiscordJS :: ${Discord.version}
+Arch :: ${process.arch}
+Platform :: ${process.platform}
+
+= Host Information =
+UsedHeap :: ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100} MB
+Heap :: ${Math.round(process.memoryUsage().heapTotal / 1024 / 1024 * 100) / 100} MB
+Ram :: ${ramUsage} MB
+Rss :: ${Math.round(process.memoryUsage().rss / 1024 / 1024 * 100) / 100} MB
+\`\`\``);
+  }
+});
+
 
 client.login(process.env.BOT_TOKEN); 
